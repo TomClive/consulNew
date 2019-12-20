764962cf0ef602ce70117c1c403644eb3c883b73
TomClive <tomclive@gmail.com>
2019-12-20 10:07:50 +0000

    Verification changes including form and ability to upload postcode csv

--- app/models/user.rb
+++ app/models/user.rb
@@ -78,7 +78,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, if: :username_required?
   validates :username, uniqueness: { scope: :registering_with_oauth }, if: :username_required?
-  validates :document_number, uniqueness: { scope: :document_type }, allow_nil: true
+  validates :document_number, uniqueness: { scope: :document_type }, allow_blank: true
 
   validate :validate_username_length
 
